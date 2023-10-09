7f53dfd753474cb404f1a2959885b2bbac5d7ccb
Martin Cardozo <tiin.-@hotmail.es>
2023-10-09 16:50:02 -0300

    se agrega ejercicio numero 4

---
+++ ejercicio4/src/App.jsx
@@ -0,0 +1,59 @@
+import { useRef, useState } from "react";
+import { List } from "./List";
+import { generateRandomId } from "./Generardor";
+
+
+function App() {
+  const [list, setList] = useState([]);
+
+  const tareaRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const tareaInput = tareaRef.current;
+    const tareaText = tareaInput.value;
+
+    if (tareaText) {
+      setList((prevList) => [
+        ...prevList,
+        { id: generateRandomId(), text: tareaText },
+      ]);
+      tareaInput.value = "";
+    }
+  };
+
+  return (
+    <>
+      <section className="d-flex justify-content-center">
+        <article className="text-center">
+          <h1 className="text-white">Bienvenido</h1>
+          <h3 className="text-white">Ingrese su lista</h3>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <input
+                type="text"
+                placeholder="Agregue su tarea"
+                id="listInput"
+                className="form-control w-100 text-center mb-3"
+                ref={tareaRef}
+              />
+            </fieldset>
+            <button type="submit" className="btn btn-success">
+              Agregar
+            </button>
+          </form>
+          <div className="container pt-5">
+          <ul className="list-group ">
+            {list.map((item) => (
+              <List key={item.id} item={item} setList={setList} />
+            ))}
+          </ul>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export default App;
