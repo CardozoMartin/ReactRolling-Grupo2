f0e0d72536cb108e9f63dd1d632578b0a53203e3
Bianca Deblinger <biandeb96@gmail.com>
2023-10-18 22:23:57 -0300

    agrego mas css

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="section-footer">Footer</div>
+    <div className="section-footer fw-bold">&copy; Rolling Code School 2023</div>
   )
 }
 export default Footer;
